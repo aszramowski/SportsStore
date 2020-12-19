e2dc19c5dbc4c3ee8121ca61d01fac5ce6ebc1bb
Artur Szramowski <artur.szramowski@wp.pl>
2020-12-19 11:09:30 +0100

    Application template

---
+++ SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Controllers;
+using Xunit;
+using System.Linq;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_paginate()
+        {
+            // Arange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductId = 1, Name = "P1" },
+                new Product { ProductId = 2, Name = "P2" },
+                new Product { ProductId = 3, Name = "P3" },
+                new Product { ProductId = 4, Name = "P4" },
+                new Product { ProductId = 5, Name = "P5" }
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
--- SportsStore/SportsStore/Controllers/ProductController.cs
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -4,20 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
-        private IProductRepository repository;
+        private IProductRepository repository;           
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+        public ViewResult List(int productPage = 1)
         {
-            return View(repository.Products);
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                        .OrderBy(p => p.ProductId)
+                        .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
         }
     }
 }
